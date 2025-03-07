9c3d6fab09f081a5522ed18e22dcc65f12088a32
Ernest HAGUMA KWIZERA <kwizeraernez@gmail.com>
2025-03-07 15:45:08 +0200

    Initializing DEVNEX HiTech Repo

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+
+const App = () => {
+
+  return (
+    <>
+      <p className="text-center text-2xl">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
